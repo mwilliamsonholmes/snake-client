e16dcb5b88231420c169cb3003b616b2008e9c2e
Melissa <mawilliamson10@gmail.com>
2022-01-20 02:31:52 +0000

    sent command strings to server using the connection object

--- input.js
+++ input.js
@@ -1,4 +1,8 @@
-const setupInput = function () {
+// Stores the active TCP connection object.
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -10,10 +14,23 @@ const setupInput = function () {
 
 
 
-const handleUserInput = function () {
+const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
+  };
+  if (key === "w") {
+    connection.write("Move: up");
+  };
+  if (key === "a") {
+    connection.write("Move: left");
+  };
+  if (key === "s") {
+    connection.write("Move: down");
+  };
+  if (key === "d") {
+    connection.write("Move: right");
   }
-};
+}
 
-module.exports = { setupInput }
+
+module.exports = { setupInput };
