992855a349361b44c7c2084184d68971c55a9eaa
Melissa <mawilliamson10@gmail.com>
2022-01-20 01:39:33 +0000

    now requires setupInput function

--- play.js
+++ play.js
@@ -1,19 +1,8 @@
 const net = require("net");
-const connect = require('./client');
+const { connect } = require('./client');
+const { setupInput } = require("./input");
 
+console.log("Connecting...");
 connect();
+setupInput();
 
-const handleUserInput = function () {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
